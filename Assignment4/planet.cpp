aaff3c86abc2d272620d4736e156018b0bb99e9e
NardeenH <97866278+NardeenH@users.noreply.github.com>
2024-04-29 09:17:52 -0500

    Update planet.cpp

--- Assignment4/planet.cpp
+++ Assignment4/planet.cpp
@@ -1,4 +1,10 @@
-//Assignment 6 planet
+//In this assignment you will be creating a base class and derived class to perform some basic calculations for planets.
+
+//For the base class, create a class for a sphere.  This class will store the radius of the sphere (private).  It will need to have an accessor and mutator for the radius.  You class should also have a constructor and functions to calculate surface area and volume.
+
+//our planet class will be derived from your sphere class.  It should add mass as a private member (with mutator and accessor) along with a constructor and functions to calculate density and acceleration due to gravity at the surface.  Functions for input and display are optional.
+
+//Using this class, write a program that will prompt the user for the radius and mass of a planet.  Your program will display the surface area, density and acceleration due to gravity at the surface.
 
 #include <iostream>
 #include <cmath>
